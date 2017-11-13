89bea3909d65858567a7b6b0ad0b1f165488cad8
github username <github email address>
2017-11-12 19:56:57 -0500

    landing page styling done

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import '../App.css';
+import '../icons8-money_bag.png';
 
 const Welcome = (props) => {
   return (
     <div>
-      <p>Welcome</p>
-      <button onClick={props.setDemoUser} type="button">Demo</button>
+      <div className="navbar">
+        <a href="#home">Budgeteer</a>
+        
+      </div>
+
+      <div className= "img-container">
+        <div className= "layer"/>
+        <h1 className= "banner"> Stop going overbudget. </h1>
+        <p className= "banner"> Knowing where your money is going is a good thing. We track and visualize your monthly budget so that you dont have to. </p>
+      </div>
+
+        <div className="bottom">
+            <button className="button button1"onClick={props.setDemoUser} type="button">Demo</button>
+        </div>
+
+        <div className="img-body-container">
+
+        </div>
+        <div className="logo"/>
     </div>
   )
 }
